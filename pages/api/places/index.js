d2e87f63920694e810368912c80f5169837dd413
Alina Rubart <rubartalina@gmail.com>
2023-09-27 15:25:10 +0800

    add read data and create form

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,4 +1,5 @@
 import dbConnect from "@/db/dbConnect";
+import Place from "@/db/models/Place";
 
 export default async function handler(request, response) {
   await dbConnect();
@@ -6,5 +7,16 @@ export default async function handler(request, response) {
   if (request.method === "GET") {
     const places = await Place.find();
     return response.status(200).json(places);
+  }
+
+  if (request.method === "POST") {
+    try {
+      const places = request.body;
+      await Place.create(places);
+
+      response.status(201).json({ status: "Place created" });
+    } catch (error) {
+      response.status(400).json({ error: error.message });
+    }
   }
 }
