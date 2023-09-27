d882d4817762cf093ade4d9ca285165f10588e2e
Alina Rubart <rubartalina@gmail.com>
2023-09-27 16:51:30 +0800

    Update Entry and Delete Entry, worked on Mareike's comments.

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -5,19 +5,28 @@ export default async function handler(request, response) {
   await dbConnect();
   const { id } = request.query;
 
-  if (request.method === "GET") {
-    const places = await Place.findById(id);
-  }
-
   if (!id) {
     return;
   }
 
-  const place = places.find((place) => place.id === id);
+  if (request.method === "GET") {
+    const place = await Place.findById(id);
+  }
 
   if (!place) {
     return response.status(404).json({ status: "Not found" });
   }
 
   response.status(200).json(place);
+
+  if (request.method === "PUT") {
+    const updatedPlace = request.body;
+    await Place.findByIdAndUpdate(id, updatedPlace);
+    response.status(200).json({ status: "Place updated." });
+  }
+
+  if (request.method === "DELETE") {
+    await Place.findByIdAndDelete(id);
+    response.status(200).json({ status: "Place deleted." });
+  }
 }
